c97c13fda31dd1cc00839c81a1ea4c7ec5169166
Lucas Badico <lucasgomesdesantana@gmail.com>
2021-08-07 16:07:52 -0300

    chore: add api test

---
+++ api-doc-cache/go.mod
@@ -0,0 +1,12 @@
+module bitbucket.org/zemoteam/api-doc-cache
+
+go 1.15
+
+require (
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/badico-cloud-hub/battery-go v0.0.0-20210807190057-073954475c1e
+	github.com/gofiber/fiber/v2 v2.16.0
+	github.com/klauspost/compress v1.13.3 // indirect
+	github.com/valyala/fasthttp v1.28.0 // indirect
+	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+)
---
+++ api-doc-cache/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+
+	batterygo "github.com/badico-cloud-hub/battery-go/battery"
+	"github.com/badico-cloud-hub/battery-go/storages"
+	"github.com/gofiber/fiber/v2"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func updateBatteryStorage() []batterygo.BatteryArgument {
+	Key := "foo"
+	Value := randSeq(10)
+	fmt.Println("update storage: ", Value)
+
+	b := batterygo.BatteryArgument{
+		Key,
+		Value,
+	}
+
+	return []batterygo.BatteryArgument{b}
+}
+func main() {
+	app := fiber.New()
+
+	storage := storages.New()
+	battery := batterygo.NewBattery(storage, 3)
+	go battery.Init(updateBatteryStorage)
+
+	// GET /api/register
+	// app.Get("/api/*", func(c *fiber.Ctx) error {
+	// 	msg := fmt.Sprintf("✋ %s", c.Params("*"))
+	// 	return c.SendString(msg) // => ✋ register
+	// })
+
+	// // GET /flights/LAX-SFO
+	// app.Get("/flights/:from-:to", func(c *fiber.Ctx) error {
+	// 	msg := fmt.Sprintf("💸 From: %s, To: %s", c.Params("from"), c.Params("to"))
+	// 	return c.SendString(msg) // => 💸 From: LAX, To: SFO
+	// })
+
+	// // GET /dictionary.txt
+	// app.Get("/:file.:ext", func(c *fiber.Ctx) error {
+	// 	msg := fmt.Sprintf("📃 %s.%s", c.Params("file"), c.Params("ext"))
+	// 	return c.SendString(msg) // => 📃 dictionary.txt
+	// })
+
+	// // GET /john/75
+	// app.Get("/:name/:age/:gender?", func(c *fiber.Ctx) error {
+	// 	msg := fmt.Sprintf("👴 %s is %s years old", c.Params("name"), c.Params("age"))
+	// 	return c.SendString(msg) // => 👴 john is 75 years old
+	// })
+
+	// GET /john
+	app.Get("read/:name", func(c *fiber.Ctx) error {
+		fmt.Println("batteryTestRead")
+		value, err := storage.Get(c.Params("name"))
+		fmt.Println("storage", storage)
+		if err != nil {
+			fmt.Println("err", err)
+			return err
+		}
+
+		msg := fmt.Sprintf("Hello, %s 👋! Your value is %s", c.Params("name"), value)
+		return c.SendString(msg) // => Hello john 👋!
+	})
+	app.Get("write/:name/:value", func(c *fiber.Ctx) error {
+		fmt.Println(c.Params("name"), c.Params("value"))
+		arg := batterygo.BatteryArgument{
+			Key:   c.Params("name"),
+			Value: c.Params("value"),
+		}
+		fmt.Println("storage", storage)
+		battery.Dispatch <- []batterygo.BatteryArgument{arg}
+		msg := fmt.Sprintf("Hello, %s 👋! Your value is %s", c.Params("name"), c.Params("value"))
+		return c.SendString(msg) // => Hello john 👋!
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		fmt.Println("storage", storage)
+
+		msg := fmt.Sprintf("Hello", c.Params("name"), c.Params("value"))
+		return c.SendString(msg) // => Hello john 👋!
+	})
+
+	log.Fatal(app.Listen(":3001"))
+}
