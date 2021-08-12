e1785089e065218272974d3d61c358a9b823b9f3
Italo Feitosa <italo85199@gmail.com>
2021-08-11 21:15:42 -0300

    feat(storages/interface): add StorageGet interface

--- storages/interface.go
+++ storages/interface.go
@@ -1,7 +1,10 @@
 package storages
 
-type Storage interface {
+type StorageGet interface {
 	Get(string) (interface{}, error)
+}
+type Storage interface {
+	StorageGet
 	// GetAll() ([]interface{}, error)
 	// Flush() error
 	Set(string, interface{}) error
